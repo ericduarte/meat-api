7e7cc697d24bf95a07aeb92b0b19b42cf522ec95
Eric Duarte <ericduartemail@gmail.com>
2018-06-01 07:35:50 -0300

    Exclusao file from gitignore

--- dist/common/environment.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.environment = {
-    server: { port: process.env.SERVER_PORT || 3001 },
-    db: { url: process.env.DB_URL || 'mongodb://localhost/meat-api' },
-    security: { saltRounds: process.env.SALT_ROUNDS || 10 }
-};
