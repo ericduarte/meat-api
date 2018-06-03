47524d717e1c63ee56f2122711419b38325842ac
Eric Duarte <ericduartemail@gmail.com>
2018-06-03 07:10:18 -0300

    Tests

--- users/users.model.ts
+++ users/users.model.ts
@@ -4,11 +4,17 @@ import {validateCPF} from '../common/validators'
 import * as bcrypt from  'bcrypt'
 import {environment} from '../common/environment'
 
+
 export interface User extends mongoose.Document{
     name:string,
     email:string,
     password:string
 }
+
+export interface UserModel extends  mongoose.Model<User>{
+    findByEmail(email:string):Promise<User>   
+}
+
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -19,8 +25,8 @@ const userSchema = new mongoose.Schema({
     },
     email:{
         type:String,
-        unique: true,
-        required:true,
+        //unique: true,
+        required:true//,
         //match:/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/
     },
     password:{
@@ -43,22 +49,44 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+userSchema.statics.findByEmail = function(email: string){
+    return this.findOne({email})
+}
+
+
+const hashPassword = (obj, next) => {
+    //ele criptografa 10 vezes dificultando a quebra a senha
+    bcrypt.hash(obj.password,environment.security.saltRounds)
+      .then(hash=>{
+          obj.password = hash
+          next()
+      })
+}
+
+
 /*É necessário usar function ao invés de arrow function
   pois o this pois caso contrário o this representará
   a função ao invés do documento ou query
 */
-userSchema.pre('save',function(next){
-  const user: User = this
-  if(!user.isModified('password')){
-    next()
-  }else{
-    //ele criptografa 10 vezes dificultando a quebra a senha
-    bcrypt.hash(user.password,environment.security.saltRounds)
-      .then(hash=>{
-          user.password = hash
-          next()
-      })
+const saveMiddleware = function(next){
+    const user: User = this
+    if(!user.isModified('password')){
+      next()
+    }else{
+      hashPassword(user, next)
+    }
   }
-})
 
-export const User = mongoose.model<User>('User',userSchema)
+const updateMiddleware = function(next){
+    if(!this.getUpdate().password){
+      next()
+    }else{
+        hashPassword(this.getUpdate(), next)
+    }
+  }
+
+userSchema.pre('save',saveMiddleware)
+userSchema.pre('findOneAndUpdate',updateMiddleware)
+userSchema.pre('update',updateMiddleware)
+
+export const User = mongoose.model<User,UserModel>('User',userSchema)
--- users/users.router.ts
+++ users/users.router.ts
@@ -1,69 +1,47 @@
-import {Router} from '../common/router'
+import {ModelRouter} from '../common/model-router'
 import * as restify from 'restify'
 import {NotFoundError} from 'restify-errors'
 import {User} from './users.model'
 
-class UsersRouter extends Router{
-    constructor(){
-       super()
-       this.on('beforeRender', document=>{
-          document.password =  undefined
-       })
+
+class UsersRouter extends ModelRouter<User> {
+
+  constructor(){
+    super(User)
+    this.on('beforeRender', document=>{
+      document.password = undefined
+      //delete document.password
+    })
+  }
+
+  findByEmail = (req, resp, next)=>{
+    if(req.query.email){
+      User.findByEmail(req.query.email)          
+          .then(user => {
+            if(user){
+              return [user]
+            } else {
+              return []  
+            }
+          })
+          .then(this.renderAll(resp, next))
+          .catch(next)
+    }else{
+      next()
     }
+  }
 
-    applyRoutes(application: restify.Server){
-        application.get('/users',(req,resp,next)=>{
-            User.find().then(this.render(resp,next))
-            .catch(next)
-        })
+  applyRoutes(application: restify.Server){
 
-        application.get('/users/:id',(req,resp,next)=>{
-            User.findById(req.params.id)
-                .then(this.render(resp,next))            
-                .catch(next)
-        })
+    //application.get({path:`${this.basePath}`, version: '2.0.0'}, [this.findByEmail, this.findAll])
+    application.get(`${this.basePath}`, this.findAll)
+    application.get(`${this.basePath}/:id`, [this.validateId, this.findById])
+    application.post(`${this.basePath}`, this.save)
+    application.put(`${this.basePath}/:id`, [this.validateId,this.replace])
+    application.patch(`${this.basePath}/:id`, [this.validateId,this.update])
+    application.del(`${this.basePath}/:id`, [this.validateId,this.remove])
 
-        application.post('/users',(req,resp,next)=>{
-            let user = new User(req.body)            
-            user.save()
-            .then(this.render(resp,next))            
-            .catch(next)
-        })
-        
-
-        application.put('/users/:id',(req,resp,next)=>{
-            const options = {override: true}
-            User.update({_id:req.params.id}, req.body, options)
-                .exec().then(result =>{
-                    if (result.n){
-                        return User.findById(req.params.id)
-                    }else{
-                        throw new NotFoundError('Documento não encontrado')
-                    }
-            })
-            .then(this.render(resp,next))
-            .catch(next);                    
-
-        })
-
-        application.patch('/users/:id',(req,resp,next)=>{
-            const options= {new : true}
-            User.findByIdAndUpdate(req.params.id, req.body, options)
-            .then(this.render(resp,next))
-            .catch(next)
-        })
-
-        application.del('/users/:id',(req,resp,next)=>{
-            User.remove({_id:req.params.id}).exec().then((cmdResult:any )=>{
-              if (cmdResult.result.n){
-                resp.send(204)
-              }else{
-                throw new NotFoundError('Documento não encontrado')
-              }
-              return next()
-            }).catch(next)        
-        })
-    }
+  }
 }
 
 export const usersRouter = new UsersRouter()
